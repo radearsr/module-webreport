39740ba2cd65f0d218257026c3b32eed78f621dc
radearsr <radea.surya4@gmail.com>
2023-07-18 21:07:20 +0700

    add new service kopNus.services.js, and add puppeteer to login kepoo

---
+++ puppeteer.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  // Perform actions on the page
+  await page.goto('https://kepooh2h.com/login', { waitUntil: "networkidle2" });
+
+  const usernameSelector = "input[name='username']";
+  const passwordSelector = "input[name='password']";
+  const loginButtonSelector = "button.login100-form-btn";
+
+
+  await page.waitForSelector(usernameSelector);
+  await page.type(usernameSelector, "H2O0599");
+  await page.waitForSelector(passwordSelector);
+  await page.type(passwordSelector, "175758");
+
+  const htmlContent = await page.content();
+  console.log(htmlContent);
+  
+  await page.waitForSelector(loginButtonSelector);
+  await page.click(loginButtonSelector);
+
+
+  // Close the browser
+  // await browser.close();
+})();
---
+++ services/kopNus.services.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const utilsHotspotReload = require("../utils/hotspotReload/hotspot.utils");
+
+const postLoginWeb = async (username, password) => {
+  const payload = utilsHotspotReload.getEncryptedPayload(username, password);
+  const config = {
+    method: "POST",
+    url: "http://103.78.38.75:8082/auth/authenticate",
+    headers: {
+      "accept": "application/json, text/javascript, */*; q=0.01",
+      "accept-language": "en-US,en;q=0.9",
+      "x-requested-with": "XMLHttpRequest",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "mode": "cors",
+      "credentials": "omit"
+    },
+    data: payload,
+  };
+  const { data: responseData } = await axios(config);
+  return responseData;
+};
+
+const getPriceLists = async () => {
+  const config = {
+    method: "GET",
+    url: "http://103.78.38.75:8082/api/pricelist",
+    headers: {
+      "accept": "application/json, text/javascript, */*; q=0.01",
+      "accept-language": "en-US,en;q=0.9",
+      "authorization": `Bearer ${token}`,
+      "x-requested-with": "XMLHttpRequest",
+      "Referer": "http://103.78.38.75:8082/daftarharga",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    }
+  };
+  const { data: responseData } = await axios(config);
+  return responseData;
+};
+
+module.exports = {
+  postLoginWeb,
+  getPriceLists,
+};
