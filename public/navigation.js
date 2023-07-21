acc46b320d9ed23962a2f4ac29de9531d9515dcf
radearsr <radea.surya4@gmail.com>
2023-07-21 22:59:11 +0700

    delete cdn alpinejs, fixing ui, finishing animation in sidebar, and finishing all ui

---
+++ public/navigation.js
@@ -0,0 +1,55 @@
+const navbarItems = document.querySelectorAll(".nav-item");
+const navbar = document.querySelector("nav");
+const sections = document.querySelectorAll("section");
+
+const setSectionActive = (currentPage, className) => {
+  sections.forEach((section) => {
+    if (currentPage === section.id) {
+      section.classList.remove(className);
+    } else {
+      section.classList.add(className);
+    }
+  });
+};
+
+const setNavbarItemActive = (anchorEl, bgClassName, fgDefault, fgActive) => {
+  navbarItems.forEach((navbarItem) => {
+    navbarItem.querySelector("a").classList.remove(bgClassName);
+    anchorEl.querySelectorAll("span")[1].classList.remove(fgDefault);
+  });
+  anchorEl.classList.add(bgClassName);
+  anchorEl.querySelectorAll("span")[1].classList.add(fgActive);
+};
+
+const setTextItemActive = (activeSpan, fgDefault, fgActive) => {
+  const spans = navbar.querySelectorAll("span:last-of-type");
+  spans.forEach((span) => {
+    span.classList.remove(fgDefault);
+  });
+  spans.forEach((span) => {
+    span.classList.remove(fgActive);
+    span.classList.add(fgDefault);
+  });
+  activeSpan.classList.add(fgActive);
+};
+
+const setSvgItemActive = (activeSvg, fillDefault, fillActive) => {
+  const svgs = navbar.querySelectorAll("svg");
+  svgs.forEach((svg) => {
+    svg.setAttribute("fill", fillDefault);
+  });
+  activeSvg.setAttribute("fill", fillActive);
+};
+
+navbarItems.forEach((navbarItem) => {
+  navbarItem.addEventListener("click", () => {
+    const currentPage = navbarItem.querySelector("a").getAttribute("href").split("#")[1];
+    const anchorEl = navbarItem.querySelector("a");
+    const currentSvg = navbarItem.querySelector("svg");
+    const currentPageSpan = anchorEl.querySelector("span:last-of-type");
+    setNavbarItemActive(anchorEl, "bg-lime-500");
+    setTextItemActive(currentPageSpan, "text-neutral-500", "text-white");
+    setSvgItemActive(currentSvg, "rgb(163 230 53)", "rgb(255 255 255)");
+    setSectionActive(currentPage, "hidden");
+  });
+});
