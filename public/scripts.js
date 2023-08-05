99f82fc790c4c3dde6177eb361f43315e93b9c4d
radearsr <radea.surya4@gmail.com>
2023-08-05 16:57:52 +0700

    adding scroll to top

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,22 @@
+const mainEl = document.querySelector("main");
+mainEl.addEventListener("scroll", () => {
+  const buttonToTop = document.getElementById("scrollToTopBtn");
+  const scrollPosition = mainEl.scrollY || mainEl.scrollTop;
+  buttonToTop.addEventListener("click", () => {
+    mainEl.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  })
+  if (scrollPosition >= 500) {
+    buttonToTop.classList.remove("hidden");
+    buttonToTop.classList.add("flex");
+  } else {
+    buttonToTop.classList.remove("flex");
+    buttonToTop.classList.add("hidden");
+  }
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   electronAPI.reqLoginStatus();
 });
-
